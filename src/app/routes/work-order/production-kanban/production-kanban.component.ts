d14abda39aad7e8ff517f1e3415b25f0cf0a41f1
Zhang Gary <garyzhangscm@gmail.com>
2021-08-02 11:47:29 -0700

    upgrade table to ng alain:st

--- src/app/routes/work-order/production-kanban/production-kanban.component.ts
+++ src/app/routes/work-order/production-kanban/production-kanban.component.ts
@@ -1,18 +1,14 @@
-import { ElementRef } from '@angular/core';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
-import { STColumn, STData } from '@delon/abc/st';
-import { _HttpClient } from '@delon/theme';
+import { Inject } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, } from '@angular/core';
+import { STColumn, } from '@delon/abc/st';
+import { ALAIN_I18N_TOKEN, _HttpClient } from '@delon/theme';
 import { NzMessageService } from 'ng-zorro-antd/message';
-import { CountdownComponent, CountdownEvent } from 'ngx-countdown';
 import { interval, Subscription } from 'rxjs';
-import { ColumnItem } from '../../util/models/column-item';
-import { UtilService } from '../../util/services/util.service';
 import { ProductionLineKanbanData } from '../../work-order/models/production-line-kanban-data';
 import { ProductionLineKanbanService } from '../../work-order/services/production-line-kanban.service';
 import { ProductionLine } from '../models/production-line';
 import { ProductionLineService } from '../services/production-line.service';
-import { STModule } from '@delon/abc/st';
-import { NzModalService } from 'ng-zorro-antd/modal';
+import { I18NService } from '@core';
 
 
 
@@ -30,151 +26,153 @@ export class WorkOrderProductionKanbanComponent implements OnInit {
   showProductionLineSet = new Set<string>();
   hideProductionLineSelection = true;
 
-
-  listOfColumns: ColumnItem[] = [
-    {
-      name: 'production-line.name',
-      showSort: true,
-      sortOrder: null,
-      sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableString(a.productionLineName, b.productionLineName),
-      sortDirections: ['ascend', 'descend'],
-      filterMultiple: true,
-      listOfFilter: [],
-      filterFn: null,
-      showFilter: false,
-    },
-    {
-      name: 'work-order.number',
-      showSort: true,
-      sortOrder: null,
-      sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableString(a.workOrderNumber, b.workOrderNumber),
-      sortDirections: ['ascend', 'descend'],
-      filterMultiple: true,
-      listOfFilter: [],
-      filterFn: null,
-      showFilter: false,
-    },
-    {
-      name: 'item.name',
-      showSort: true,
-      sortOrder: null,
-      sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableString(a.itemName, b.itemName),
-      sortDirections: ['ascend', 'descend'],
-      filterMultiple: true,
-      listOfFilter: [],
-      filterFn: null,
-      showFilter: false,
-    },
-    {
-      name: 'production-line.model',
-      showSort: true,
-      sortOrder: null,
-      sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableString(a.productionLineModel!, b.productionLineModel!),
-      sortDirections: ['ascend', 'descend'],
-      filterMultiple: true,
-      listOfFilter: [],
-      filterFn: null,
-      showFilter: false,
-    },
-    {
-      name: 'production-line.target-production',
-      showSort: true,
-      sortOrder: null,
-      sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.productionLineTargetOutput, b.productionLineTargetOutput),
-      sortDirections: ['ascend', 'descend'],
-      filterMultiple: true,
-      listOfFilter: [],
-      filterFn: null,
-      showFilter: false,
-    },
-    {
-      name: 'production-line.actual-production',
-      showSort: true,
-      sortOrder: null,
-      sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.productionLineActualOutput, b.productionLineActualOutput),
-      sortDirections: ['ascend', 'descend'],
-      filterMultiple: true,
-      listOfFilter: [],
-      filterFn: null,
-      showFilter: false,
-    },
-    {
-      name: 'production-line.actual-putaway-production',
-      showSort: true,
-      sortOrder: null,
-      sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.productionLineActualPutawayOutput, b.productionLineActualPutawayOutput),
-      sortDirections: ['ascend', 'descend'],
-      filterMultiple: true,
-      listOfFilter: [],
-      filterFn: null,
-      showFilter: false,
-    },
-    {
-      name: 'production-line.target-total-production',
-      showSort: true,
-      sortOrder: null,
-      sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.productionLineTotalTargetOutput, b.productionLineTotalTargetOutput),
-      sortDirections: ['ascend', 'descend'],
-      filterMultiple: true,
-      listOfFilter: [],
-      filterFn: null,
-      showFilter: false,
-    },
-    {
-      name: 'production-line.actual-total-production',
-      showSort: true,
-      sortOrder: null,
-      sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.productionLineTotalActualOutput, b.productionLineTotalActualOutput),
-      sortDirections: ['ascend', 'descend'],
-      filterMultiple: true,
-      listOfFilter: [],
-      filterFn: null,
-      showFilter: false,
-    },
-    {
-      name: 'production-line.actual-total-putaway-production',
-      showSort: true,
-      sortOrder: null,
-      sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.productionLineTotalActualPutawayOutput, b.productionLineTotalActualPutawayOutput),
-      sortDirections: ['ascend', 'descend'],
-      filterMultiple: true,
-      listOfFilter: [],
-      filterFn: null,
-      showFilter: false,
-    },
-    {
-      name: 'status',
-      showSort: true,
-      sortOrder: null,
-      sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableString(a.workOrderStatus!, b.workOrderStatus!),
-      sortDirections: ['ascend', 'descend'],
-      filterMultiple: true,
-      listOfFilter: [],
-      filterFn: null,
-      showFilter: false,
-    },
-    {
-      name: 'shift',
-      showSort: true,
-      sortOrder: null,
-      sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableString(a.shift!, b.shift!),
-      sortDirections: ['ascend', 'descend'],
-      filterMultiple: true,
-      listOfFilter: [],
-      filterFn: null,
-      showFilter: false,
-    },
-    {
-      name: 'finished-rate',
-      showSort: true,
-      sortOrder: null,
-      sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.percent!, b.percent!),
-      sortDirections: ['ascend', 'descend'],
-      filterMultiple: true,
-      listOfFilter: [],
-      filterFn: null,
-      showFilter: false,
-    },];
+  /***
+   * 
+    listOfColumns: ColumnItem[] = [
+      {
+        name: 'production-line.name',
+        showSort: true,
+        sortOrder: null,
+        sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableString(a.productionLineName, b.productionLineName),
+        sortDirections: ['ascend', 'descend'],
+        filterMultiple: true,
+        listOfFilter: [],
+        filterFn: null,
+        showFilter: false,
+      },
+      {
+        name: 'work-order.number',
+        showSort: true,
+        sortOrder: null,
+        sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableString(a.workOrderNumber, b.workOrderNumber),
+        sortDirections: ['ascend', 'descend'],
+        filterMultiple: true,
+        listOfFilter: [],
+        filterFn: null,
+        showFilter: false,
+      },
+      {
+        name: 'item.name',
+        showSort: true,
+        sortOrder: null,
+        sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableString(a.itemName, b.itemName),
+        sortDirections: ['ascend', 'descend'],
+        filterMultiple: true,
+        listOfFilter: [],
+        filterFn: null,
+        showFilter: false,
+      },
+      {
+        name: 'production-line.model',
+        showSort: true,
+        sortOrder: null,
+        sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableString(a.productionLineModel!, b.productionLineModel!),
+        sortDirections: ['ascend', 'descend'],
+        filterMultiple: true,
+        listOfFilter: [],
+        filterFn: null,
+        showFilter: false,
+      },
+      {
+        name: 'production-line.target-production',
+        showSort: true,
+        sortOrder: null,
+        sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.productionLineTargetOutput, b.productionLineTargetOutput),
+        sortDirections: ['ascend', 'descend'],
+        filterMultiple: true,
+        listOfFilter: [],
+        filterFn: null,
+        showFilter: false,
+      },
+      {
+        name: 'production-line.actual-production',
+        showSort: true,
+        sortOrder: null,
+        sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.productionLineActualOutput, b.productionLineActualOutput),
+        sortDirections: ['ascend', 'descend'],
+        filterMultiple: true,
+        listOfFilter: [],
+        filterFn: null,
+        showFilter: false,
+      },
+      {
+        name: 'production-line.actual-putaway-production',
+        showSort: true,
+        sortOrder: null,
+        sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.productionLineActualPutawayOutput, b.productionLineActualPutawayOutput),
+        sortDirections: ['ascend', 'descend'],
+        filterMultiple: true,
+        listOfFilter: [],
+        filterFn: null,
+        showFilter: false,
+      },
+      {
+        name: 'production-line.target-total-production',
+        showSort: true,
+        sortOrder: null,
+        sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.productionLineTotalTargetOutput, b.productionLineTotalTargetOutput),
+        sortDirections: ['ascend', 'descend'],
+        filterMultiple: true,
+        listOfFilter: [],
+        filterFn: null,
+        showFilter: false,
+      },
+      {
+        name: 'production-line.actual-total-production',
+        showSort: true,
+        sortOrder: null,
+        sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.productionLineTotalActualOutput, b.productionLineTotalActualOutput),
+        sortDirections: ['ascend', 'descend'],
+        filterMultiple: true,
+        listOfFilter: [],
+        filterFn: null,
+        showFilter: false,
+      },
+      {
+        name: 'production-line.actual-total-putaway-production',
+        showSort: true,
+        sortOrder: null,
+        sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.productionLineTotalActualPutawayOutput, b.productionLineTotalActualPutawayOutput),
+        sortDirections: ['ascend', 'descend'],
+        filterMultiple: true,
+        listOfFilter: [],
+        filterFn: null,
+        showFilter: false,
+      },
+      {
+        name: 'status',
+        showSort: true,
+        sortOrder: null,
+        sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableString(a.workOrderStatus!, b.workOrderStatus!),
+        sortDirections: ['ascend', 'descend'],
+        filterMultiple: true,
+        listOfFilter: [],
+        filterFn: null,
+        showFilter: false,
+      },
+      {
+        name: 'shift',
+        showSort: true,
+        sortOrder: null,
+        sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableString(a.shift!, b.shift!),
+        sortDirections: ['ascend', 'descend'],
+        filterMultiple: true,
+        listOfFilter: [],
+        filterFn: null,
+        showFilter: false,
+      },
+      {
+        name: 'finished-rate',
+        showSort: true,
+        sortOrder: null,
+        sortFn: (a: ProductionLineKanbanData, b: ProductionLineKanbanData) => this.utilService.compareNullableNumber(a.percent!, b.percent!),
+        sortDirections: ['ascend', 'descend'],
+        filterMultiple: true,
+        listOfFilter: [],
+        filterFn: null,
+        showFilter: false,
+      },];
+   */
 
   //  @ViewChild('cd', { static: false }) private countdown!: CountdownComponent; 
   // @ViewChild('countdown', { static: true }) private countdown!: CountdownComponent; 
@@ -182,11 +180,11 @@ export class WorkOrderProductionKanbanComponent implements OnInit {
 
   countDownNumber = 60;
   countDownsubscription!: Subscription;
-  constructor(private http: _HttpClient, public msg: NzMessageService, private cdr: ChangeDetectorRef,
-
-    private utilService: UtilService,
+  constructor(
+    public msg: NzMessageService, private cdr: ChangeDetectorRef,
     private productionLineKanbanService: ProductionLineKanbanService,
-    private productionLineService: ProductionLineService) {
+    private productionLineService: ProductionLineService,
+    @Inject(ALAIN_I18N_TOKEN) private i18n: I18NService) {
     this.loadProductionLines();
   }
 
@@ -223,11 +221,9 @@ export class WorkOrderProductionKanbanComponent implements OnInit {
   // switch to add or remove the productione line from display
   switchProductionLineDisplay(name: string) {
     if (this.showProductionLineSet.has(name)) {
-      console.log(`remove production line ${name}`);
       this.showProductionLineSet.delete(name);
     }
     else {
-      console.log(`add production line ${name}`);
       this.showProductionLineSet.add(name);
     }
     this.refreshKanbanData();
@@ -262,30 +258,34 @@ export class WorkOrderProductionKanbanComponent implements OnInit {
     this.hideProductionLineSelection = !this.hideProductionLineSelection;
   }
 
-  users: STData[] = Array(10)
-    .fill({})
-    .map((_, idx) => {
-      return {
-        id: idx + 1,
-        name: `name ${idx + 1}`,
-        age: Math.ceil(Math.random() * 10) + 20,
-      };
-    });
   columns: STColumn[] = [
-    { title: '编号', index: 'id' },
-    { title: '姓名', index: 'name', iif: () => this.isChoose('name') },
-    { title: '年龄', index: 'age', iif: () => this.isChoose('age') },
-    {
-      title: '自定义',
-      renderTitle: 'customTitle',
-      render: 'custom',
-      iif: () => this.isChoose('custom'),
-    },
+    { title: this.i18n.fanyi("production-line.name"), index: 'productionLineName', iif: () => this.isChoose('productionLineName'), },
+    { title: this.i18n.fanyi("work-order.number"), index: 'workOrderNumber', iif: () => this.isChoose('workOrderNumber'), },
+    { title: this.i18n.fanyi("item.name"), index: 'itemName', iif: () => this.isChoose('itemName'), },
+    { title: this.i18n.fanyi("production-line.target-production"), index: 'productionLineTargetOutput', iif: () => this.isChoose('productionLineTargetOutput'), },
+    { title: this.i18n.fanyi("production-line.actual-production"), index: 'productionLineActualOutput', iif: () => this.isChoose('productionLineActualOutput'), },
+    { title: this.i18n.fanyi("production-line.actual-putaway-production"), index: 'productionLineActualPutawayOutput', iif: () => this.isChoose('productionLineActualPutawayOutput'), },
+    { title: this.i18n.fanyi("production-line.target-total-production"), index: 'productionLineTotalTargetOutput', iif: () => this.isChoose('productionLineTotalTargetOutput'), },
+    { title: this.i18n.fanyi("production-line.actual-total-production"), index: 'productionLineTotalActualOutput', iif: () => this.isChoose('productionLineTotalActualOutput'), },
+    { title: this.i18n.fanyi("production-line.actual-total-putaway-production"), index: 'productionLineTotalActualPutawayOutput', iif: () => this.isChoose('productionLineTotalActualPutawayOutput'), },
+    { title: this.i18n.fanyi("status"), index: 'workOrderStatus', iif: () => this.isChoose('workOrderStatus'), },
+    { title: this.i18n.fanyi("shift"), index: 'shift', iif: () => this.isChoose('shift'), },
+    { title: this.i18n.fanyi("finished-rate"), index: 'percent', iif: () => this.isChoose('percent'), },
   ];
   customColumns = [
-    { label: '姓名', value: 'name', checked: true },
-    { label: '年龄', value: 'age', checked: true },
-    { label: '自定义', value: 'custom', checked: true },
+
+    { label: this.i18n.fanyi("production-line.name"), value: 'productionLineName', checked: true },
+    { label: this.i18n.fanyi("work-order.number"), value: 'workOrderNumber', checked: true },
+    { label: this.i18n.fanyi("item.name"), value: 'itemName', checked: true },
+    { label: this.i18n.fanyi("production-line.target-production"), value: 'productionLineTargetOutput', checked: true },
+    { label: this.i18n.fanyi("production-line.actual-production"), value: 'productionLineActualOutput', checked: true },
+    { label: this.i18n.fanyi("production-line.actual-putaway-production"), value: 'productionLineActualPutawayOutput', checked: true },
+    { label: this.i18n.fanyi("production-line.target-total-production"), value: 'productionLineTotalTargetOutput', checked: true },
+    { label: this.i18n.fanyi("production-line.actual-total-production"), value: 'productionLineTotalActualOutput', checked: true },
+    { label: this.i18n.fanyi("production-line.actual-total-putaway-production"), value: 'productionLineTotalActualPutawayOutput', checked: true },
+    { label: this.i18n.fanyi("status"), value: 'workOrderStatus', checked: true },
+    { label: this.i18n.fanyi("shift"), value: 'shift', checked: true },
+    { label: this.i18n.fanyi("finished-rate"), value: 'percent', checked: true },
   ];
 
   isChoose(key: string): boolean {
