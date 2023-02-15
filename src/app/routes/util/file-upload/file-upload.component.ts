8e5b72d444c30d614f245b8238083a6231f1a85a
Gary Zhang <garyzhangscm@gmail.com>
2023-02-14 19:58:59 -0800

    bug fix

--- src/app/routes/util/file-upload/file-upload.component.ts
+++ src/app/routes/util/file-upload/file-upload.component.ts
@@ -7,6 +7,8 @@ import { ALAIN_I18N_TOKEN, TitleService, _HttpClient } from '@delon/theme';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
 
+import { Warehouse } from '../../warehouse-layout/models/warehouse';
+import { WarehouseService } from '../../warehouse-layout/services/warehouse.service';
 import { FileUploadType } from '../models/file-upload-type';
 import { FileUploadOperationService } from '../services/file-upload-operation.service';
 
@@ -24,6 +26,7 @@ export class UtilFileUploadComponent implements OnInit {
     @Inject(ALAIN_I18N_TOKEN) private i18n: I18NService,
     private titleService: TitleService,
     private msg: NzMessageService,
+    private warehouseService: WarehouseService
   ) {}
   
   fileTypes = ['.csv'];
@@ -32,8 +35,14 @@ export class UtilFileUploadComponent implements OnInit {
   fromMenu = false;
   pageTitle = '';
   selectedFileUploadType?: FileUploadType;
+  selectedFileUploadUrl = '';
   fileUploadDisabled = false;
+  isSpinning = false;
 
+  // when we upload a bulky files, we will need to 
+  // use the key so we can track the progress.
+  fileUploadProgressKey = '';
+  fileUploadProgress = 100;
   removeExistingInventory = true;
 
   allowedFileTypes: Array<{ label: string; value: string }> = [];
@@ -74,6 +83,7 @@ export class UtilFileUploadComponent implements OnInit {
             
             
             this.selectedFileUploadType = fileUploadType;
+            this.selectedFileUploadUrl = `${this.selectedFileUploadType.destinationUrl}?warehouseId=${this.warehouseService.getCurrentWarehouse().id}`;
           });
       });
     }
@@ -87,15 +97,58 @@ export class UtilFileUploadComponent implements OnInit {
     this.fileUploadDisabled = false;
   }
 
-  handleChange(info: NzUploadChangeParam): void {
-    console.log('START TO HANDLE CHANGE');
-    if (info.file.status !== 'uploading') {
+  handleChange(info: NzUploadChangeParam): void {  
+    if (info.file.status === 'uploading') {
+      // this.isSpinning = true;
+      this.fileUploadProgress = 0;
       console.log(info.file, info.fileList);
     }
-    if (info.file.status === 'done') {
-      this.msg.success(`${info.file.name} file uploaded successfully`);
+    else if (info.file.status === 'done') { 
+      this.checkFileUploadProgress(info);
+      
     } else if (info.file.status === 'error') {
+      this.isSpinning = false;
+      this.fileUploadProgress = 100;
       this.msg.error(`${info.file.name} file upload failed.`);
     }
+  } 
+
+  checkFileUploadProgress(info: NzUploadChangeParam) {
+
+    if (this.selectedFileUploadType?.name == 'inventory') { 
+      
+      // this.isSpinning = true;
+      setTimeout(() => {  
+        this.fileUploadOperationService.getInventoryFileUploadProgress(info.file.response.data).subscribe({
+          next: (progress) => {
+            this.fileUploadProgress = progress;
+            if (progress >= 100) {
+              // this.isSpinning = false;
+              this.msg.success(`${info.file.name} file uploaded successfully`);
+
+            }
+            else {
+              // OK, we are not done yet, let's call the same command again
+              console.log(`${info.file.name} upload progress: ${progress}`);
+              this.checkFileUploadProgress(info);
+            }
+          }, 
+          error: () =>  this.fileUploadProgress = 100
+          //this.isSpinning = false
+        })
+      }, 1000);
+    }
+    else {
+      
+      this.fileUploadProgress = 100;
+      this.msg.success(`${info.file.name} file uploaded successfully`);
+    }
+  }
+
+  handleCancel() {
+    return false;
+  }
+  handleOk() {
+    return false;
   }
 }
--- src/app/routes/util/services/file-upload-operation.service.ts
+++ src/app/routes/util/services/file-upload-operation.service.ts
@@ -1,8 +1,10 @@
+import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
 import { Observable, of } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 
+import { WarehouseService } from '../../warehouse-layout/services/warehouse.service';
 import { FileUploadType } from '../models/file-upload-type';
 import { GzLocalStorageService } from './gz-local-storage.service';
 
@@ -10,9 +12,19 @@ import { GzLocalStorageService } from './gz-local-storage.service';
   providedIn: 'root',
 })
 export class FileUploadOperationService {
-  constructor(private http: _HttpClient, private gzLocalStorageService: GzLocalStorageService) {}
+  constructor(private http: _HttpClient, 
+    private warehouseService: WarehouseService) {}
 
   getFileUploadTypes(): Observable<FileUploadType[]> {
     return this.http.get('resource/file-upload/types').pipe(map(res => res.data));
+  }
+  
+  getInventoryFileUploadProgress(key: string): Observable<number> {
+    
+    let params = new HttpParams();
+    params = params.append("warehouseId", this.warehouseService.getCurrentWarehouse().id);
+    params = params.append("key", key);
+
+    return this.http.get(`inventory/inventories/upload/progress`, params).pipe(map(res => res.data));
   }
 }
